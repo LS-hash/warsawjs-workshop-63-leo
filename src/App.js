bbd09b2a4243b595c937a192a0e250a68d1983de
LS-hash <sobeckal@gmail.com>
2022-06-19 12:23:28 +0000

    basic structure of meme generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-import Text from './components/Text';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Capture from "./components/Capture";
 
 function App() {
+  const [file, setFile] = useState();
+  const [preview, setPreview] = useState();
+  const [text1, setText1] = useState("Text1");
+  const [text2, setText2] = useState("Text2");
+  const onFileInputChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const onTextInputChangeTop = (event) => {
+    setText1(event.target.value);
+  };
+  const onTextInputChangeBottom = (event) => {
+    setText2(event.target.value);
+  };
+
+  useEffect(() => {
+    if (!file) {
+      setPreview(undefined);
+      return;
+    }
+
+    const objectUrl = URL.createObjectURL(file);
+    setPreview(objectUrl);
+    return () => URL.revokeObjectURL(objectUrl);
+  }, [file]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Text text ='hello' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Text text ='value' />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {preview && (
+          <Capture img={preview} textTop={text1} textBottom={text2} />
+        )}
+        <input type={"file"} onChange={onFileInputChange} />
+        <input type={"text"} onChange={onTextInputChangeTop} />
+        <input type={"text2"} onChange={onTextInputChangeBottom} />
       </header>
     </div>
   );
--- src/components/Text.js
+++ src/components/Text.js
@@ -1,27 +1,24 @@
-import { useState } from "react"
+import { useState } from "react";
 
-const Text = ({text = '123', defaultParameter = 0}) => {
+const Text = ({ text = "123", defaultParameter = 0 }) => {
+  const [counter, setCounter] = useState(defaultParameter);
+  const onChange = (increment) => {
+    setCounter(counter + increment);
+  };
 
-    const [counter, setCounter] = useState(defaultParameter);
-    const onChange = (increment) => { 
-        setCounter(counter + increment);
-    }
-    
-    return (
-    <div> 
-        <p>{text}</p>
-        <h2> {counter} </h2>
-        <button onClick={() => onChange(1)}>
-            add value 1
-        </button>
-        <button onClick={() => onChange(-1)}>
-            decrement value 1
-        </button>
-        <button onClick={() => onChange(5)}>
-            add value 5
-        </button>
-    </div>)
-    
-}
+  return (
+    <div>
+      <p>{text}</p>
+      <h2> {counter} </h2>
+      <button onClick={() => onChange(1)}>add value 1</button>
+      <button onClick={() => onChange(-1)}>decrement value 1</button>
+      <button onClick={() => onChange(5)}>add value 5</button>
+    </div>
+  );
+};
 
-export default Text
+export default Text;
+
+// curly brackets = objects, parameters
+// square brackets = arrays
+// regular brackets = useState(), console.log(), properties
