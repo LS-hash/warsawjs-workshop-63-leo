74756ad19bf57f3876c28a2a51c94ceb2bfd7dc7
LS-hash <sobeckal@gmail.com>
2022-06-19 09:33:53 +0000

    basic component with state

---
+++ src/components/Text.js
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+const Text = ({text = '123', defaultParameter = 0}) => {
+
+    const [counter, setCounter] = useState(defaultParameter);
+    const onChange = (increment) => { 
+        setCounter(counter + increment);
+    }
+    
+    return (
+    <div> 
+        <p>{text}</p>
+        <h2> {counter} </h2>
+        <button onClick={() => onChange(1)}>
+            add value 1
+        </button>
+        <button onClick={() => onChange(-1)}>
+            decrement value 1
+        </button>
+        <button onClick={() => onChange(5)}>
+            add value 5
+        </button>
+    </div>)
+    
+}
+
+export default Text
